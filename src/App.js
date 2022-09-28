3e3064edb222f19e9853709951c8f04a48bbede1
Sudhansh Pal <sudpal1@Sudhanshs-MacBook-Pro.local>
2022-09-28 16:58:58 +0530

    Card component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import ApplicationHeader from './components/ApplicationHeader';
 import FilterSection from './components/FilterSection';
-import getAllUsers from './services/HttpService';
+
+import Card from './components/Card';
 
 function App() {
   const title ='Pokedex';
@@ -9,8 +10,45 @@ function App() {
 
   return (
     <div className='container-fluid  app-bg-color'>
+      <div className='row m-0'>
       <ApplicationHeader title={title} description={description} ></ApplicationHeader>
+      </div>
+      <div className='row m-0'>
       <FilterSection></FilterSection>
+      </div>
+     <div className='row m-0'>
+      <div className='col-md-2 p-3'>
+      <Card img='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg' name='spal' id='001' ></Card>
+      
+      </div>
+      <div className='col-md-2 p-3'>
+      <Card img='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg' name='spal' id='002' ></Card>
+      
+      </div>
+      <div className='col-md-2 p-3'>
+      <Card img='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg' name='spal' id='001' ></Card>
+      
+      </div>
+      <div className='col-md-2 p-3'>
+      <Card img='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/7.svg' name='spal' id='001' ></Card>
+      
+      </div>
+      <div className='col-md-2 p-3'>
+      <Card img='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/9.svg' name='spal' id='001' ></Card>
+      
+      </div>
+      <div className='col-md-2 p-3'>
+      <Card img='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/17.svg' name='spal' id='001' ></Card>
+      
+      </div>
+      <div className='col-md-2 p-3'>
+      <Card img='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/11.svg' name='spal' id='001' ></Card>
+      
+      </div>
+      
+     </div>
+      
+      
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,15 @@
-
+import './Card.css';
 
 function Card(props) {
-    const classes = 'card' + props.className;
     return (
-        <div className={classes}>{props.children}</div>
+        <div className="card card-border align-items-center justify-content-center" >
+            <img src={props.img} className="card-img-top img-size" alt="..." />
+            <div className="card-body">
+                <p className="text-center m-0 fw-bold">{props.name}</p>
+                <p className="text-center m-0">{props.id}</p>
+            </div>
+        </div>
+
     )
 }
 

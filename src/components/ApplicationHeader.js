187a947b8367dccdd7af4a2efdf1da148493399e
Sudhansh Pal <sudpal1@Sudhanshs-MacBook-Pro.local>
2022-09-28 18:07:01 +0530

    header & filter section

--- src/components/ApplicationHeader.js
+++ src/components/ApplicationHeader.js
@@ -3,8 +3,9 @@ import './ApplicationHeader.css';
 function ApplicationHeader(props) {
     return (
         <div className='py-3'>
-             <label className='px-3 title'>{props.title}</label>
-             <div className='vr'></div>
+             <label className='px-3 col-xs-12 title'>{props.title}</label>
+             <div className='vr d-none d-sm-inline-block'></div>
+             <hr className='hr d-block d-sm-none'/>
              <label className='px-3 description'>{props.description}</label>
         </div>
         );
--- src/components/FilterSection.js
+++ src/components/FilterSection.js
@@ -5,19 +5,19 @@ function FilterSection() {
         <div className="row m-0">
             <div className="col-md-6">
                 <label className="filter-label">Search By</label>
-                <input class="form-control control-bg-color" placeholder="Name or Number" />
+                <input class="form-control fc-height control-bg-color" placeholder="Name or Number" />
             </div>
             <div className="col-md-2">
                 <label className="filter-label">Type</label>
-                <input class="form-control control-bg-color" />
+                <input class="form-control fc-height control-bg-color" />
             </div>
             <div className="col-md-2">
                 <label className="filter-label">Gender</label>
-                <input class="form-control control-bg-color" />
+                <input class="form-control fc-height control-bg-color" />
             </div>
             <div className="col-md-2">
                 <label className="filter-label">Stats</label>
-                <input class="form-control control-bg-color" />
+                <input class="form-control fc-height control-bg-color" />
             </div>
         </div>);
 }

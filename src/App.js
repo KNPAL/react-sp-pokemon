06fc0ced6898b4436bda930277f9975f1400efa6
Sudhansh Pal <sudpal1@Sudhanshs-MacBook-Pro.local>
2022-09-29 00:51:51 +0530

    fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,54 +1,57 @@
 import './App.css';
 import ApplicationHeader from './components/ApplicationHeader';
 import FilterSection from './components/FilterSection';
-
+import arrayList from './services/HttpService';
+import Appstring from './utiles/Constants';
 import Card from './components/Card';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const title ='Pokedex';
+  const title = 'Pokedex';
   const description = 'Search for any Pokemon that exists on the planet';
+
+  const [pokemonList, setPokemons] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function getAllPokemon() {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error('Something went woung')
+      }
+      setPokemons(data.results);
+    } catch (err) {
+      setError(err.message)
+    }
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    getAllPokemon()
+  }, []);
 
   return (
     <div className='container-fluid  app-bg-color'>
       <div className='row m-0'>
-      <ApplicationHeader title={title} description={description} ></ApplicationHeader>
+        <ApplicationHeader title={title} description={description} ></ApplicationHeader>
       </div>
       <div className='row m-0'>
-      <FilterSection></FilterSection>
+        <FilterSection></FilterSection>
       </div>
-     <div className='row m-0'>
-      <div className='col-md-2 p-3'>
-      <Card img='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg' name='spal' id='001' ></Card>
-      
-      </div>
-      <div className='col-md-2 p-3'>
-      <Card img='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg' name='spal' id='002' ></Card>
-      
-      </div>
-      <div className='col-md-2 p-3'>
-      <Card img='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg' name='spal' id='001' ></Card>
-      
-      </div>
-      <div className='col-md-2 p-3'>
-      <Card img='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/7.svg' name='spal' id='001' ></Card>
-      
-      </div>
-      <div className='col-md-2 p-3'>
-      <Card img='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/9.svg' name='spal' id='001' ></Card>
-      
-      </div>
-      <div className='col-md-2 p-3'>
-      <Card img='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/17.svg' name='spal' id='001' ></Card>
-      
-      </div>
-      <div className='col-md-2 p-3'>
-      <Card img='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/11.svg' name='spal' id='001' ></Card>
-      
-      </div>
-      
-     </div>
-      
-      
+      {!isLoading && <div className='row m-0 py-3'>
+        {pokemonList?.map((props, key) => (
+          <div className='col-md-2 p-3'>
+            <Card
+              img={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${key + 1}.svg`}
+              name={props.name} id={key} />
+          </div>
+        ))}
+      </div>}
+      {isLoading && <p>Loading...</p>}
     </div>
   );
 }
---
+++ src/utiles/Constants.js
@@ -0,0 +1,31 @@
+const Appstring = {
+    title: 'Pokedex',
+    description: 'Search for any Pokemon that exists on the planet'
+}
+
+
+const AppColor = {
+    Normal: '#DDCBD0',
+    Fighting: '#FCC1B0',
+    Flying: '#B2D2E8',
+    Poison: '#CFB7ED',
+    Ground: '#F4D1A6',
+    Rock: '#C5AEA8',
+    Bug: '#C1E0C8',
+    Ghost: '#D7C2D7',
+    Steel: '#C2D4CE',
+    Fire: '#EDC2C4',
+    Water: '#CBD5ED',
+    Grass: '#C0D4C8',
+    Dragon: '#CADCDF',
+    Drak: '#C6C5E3',
+    Electric: '#E2E2A0',
+    Fairy: '#E4C0CF',
+    Psychic: '#DDC0CF',
+    Unknown: '#C0DFDD',
+    Ice: '#C7D7DF',
+    Shadow: '#CACACA'
+}
+
+
+export default { AppColor, Appstring };

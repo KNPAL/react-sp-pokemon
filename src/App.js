623818cb76d9e98d95f69c3ff442df5b179e9adb
Sudhansh Pal <sudpal1@Sudhanshs-MacBook-Pro.local>
2022-09-29 11:42:15 +0530

    pokemon list

--- src/App.js
+++ src/App.js
@@ -1,57 +1,24 @@
 import './App.css';
 import ApplicationHeader from './components/ApplicationHeader';
 import FilterSection from './components/FilterSection';
-import arrayList from './services/HttpService';
-import Appstring from './utiles/Constants';
-import Card from './components/Card';
-import { useEffect, useState } from 'react';
+import PokemonList from './components/PokemonList';
+
 
 function App() {
   const title = 'Pokedex';
   const description = 'Search for any Pokemon that exists on the planet';
 
-  const [pokemonList, setPokemons] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  async function getAllPokemon() {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon');
-      const data = await response.json();
-      if (!response.ok) {
-        throw new Error('Something went woung')
-      }
-      setPokemons(data.results);
-    } catch (err) {
-      setError(err.message)
-    }
-    setIsLoading(false);
-  }
-
-  useEffect(() => {
-    getAllPokemon()
-  }, []);
-
   return (
     <div className='container-fluid  app-bg-color'>
       <div className='row m-0'>
-        <ApplicationHeader title={title} description={description} ></ApplicationHeader>
+        <ApplicationHeader title={title} description={description} />
       </div>
       <div className='row m-0'>
-        <FilterSection></FilterSection>
+        <FilterSection />
       </div>
-      {!isLoading && <div className='row m-0 py-3'>
-        {pokemonList?.map((props, key) => (
-          <div className='col-md-2 p-3'>
-            <Card
-              img={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${key + 1}.svg`}
-              name={props.name} id={key} />
-          </div>
-        ))}
-      </div>}
-      {isLoading && <p>Loading...</p>}
+      <div className='row m-0'>
+        <PokemonList />
+      </div>
     </div>
   );
 }
---
+++ src/components/PokemonList.js
@@ -0,0 +1,54 @@
+
+import Card from './Card';
+import { useEffect, useState } from 'react';
+
+function PokemonList() {
+
+    const [pokemonList, setPokemons] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    async function getAllPokemon() {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+            const data = await response.json();
+            if (!response.ok) {
+                throw new Error('Something went woung')
+            }
+            const transformList = data.results.map((item) => {
+                return {
+                    name: item.name,
+                    id: item.url.substring(34, item.url.lastIndexOf('/')),
+                    imgUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${item.url.substring(34, item.url.lastIndexOf('/'))}.svg`
+                }
+            });
+            setPokemons(transformList);
+        } catch (err) {
+            setError(err.message)
+        }
+        setIsLoading(false);
+    }
+
+    useEffect(() => {
+        getAllPokemon()
+    }, []);
+
+    return (
+        <div>
+            {!isLoading && <div className='row m-0 py-3'>
+                {pokemonList?.map((props, key) => (
+                    <div className='col-md-2 p-3'>
+                        <Card
+                            img={props.imgUrl}
+                            name={props.name} id={props.id} />
+                    </div>
+                ))}
+            </div>}
+            {!error && isLoading && <p>Loading...</p>}
+        </div>
+    )
+}
+
+export default PokemonList

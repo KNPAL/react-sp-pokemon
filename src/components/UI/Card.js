ea1a053d34609be9d9d5068e61ff17d19906fae1
Sudhansh Pal <sudpal1@Sudhanshs-MacBook-Pro.local>
2022-09-30 00:34:39 +0530

    modal box stru

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -5,16 +5,24 @@ function Card(props) {
 
     const [modalShow, setModalShow] = useState(false);
 
+    const onCardClickHandler = () =>{
+        setModalShow(true)
+    }
+
+    const onCloseClickHandler = ()=>{
+        setModalShow(false);
+    }
+
     return (
         <>
-            <div onClick={() => setModalShow(true)} className="card card-border align-items-center justify-content-center" >
+            <div onClick={onCardClickHandler} className="card card-border align-items-center justify-content-center" >
                 <img src={props.img} className="card-img-top img-size" alt="..." />
                 <div className="card-body">
                     <p className="text-center m-0 fw-bold">{props.name}</p>
                     <p className="text-center m-0">{props.id}</p>
                 </div>
             </div>
-            <ModalBox name={props.name} id={props.id} show={modalShow} onHide={() => setModalShow(false)} />
+           {modalShow && <ModalBox name={props.name} id={props.id} onModalClose={onCloseClickHandler}   />} 
         </>
 
     )
--- src/components/UI/ModalBox.js
+++ src/components/UI/ModalBox.js
@@ -1,28 +1,98 @@
-import './ModalBox.css'
+import './ModalBox.css';
+import React, { useEffect, useState } from 'react';
 
-import { Modal, Button } from 'react-bootstrap';
 
 function ModalBox(props) {
-    
+
+    //#region api need to filter
+    const [pokemon, setPokemon] = useState({});
+
+    async function getPokemon() {
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${props.id}`);
+            const data = await response.json();
+            // pokemon = data;
+            setPokemon(data);
+            console.log(data);
+
+            const response1 = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${props.id}`);
+            const data1 = await response1.json();
+            console.log(data1);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        getPokemon()
+    }, []);
+
+    //#endregion
+
     return (
-        <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                     {props.name} {props.id}
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <h4>Centered Modal</h4>
-                <p>
-                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                    consectetur ac, vestibulum at eros.
-                </p>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={props.onHide}>Close</Button>
-            </Modal.Footer>
-        </Modal>
+        <>
+            <div className='backdrop'>
+                <div className="modal fade show" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" >
+                    <div className="modal-dialog modal-lg">
+                        <div className="modal-content h-100">
+                            <div className="modal-body">
+                                <div className='container h-100'>
+                                    <div className='row m-0 h-50'>
+                                        <div className='col-md-4'>
+                                            <div className='border border-dark h-100 w-100'>
+
+                                            </div>
+                                        </div>
+                                        <div className='col-md-8'>
+                                            <label className='m-3'>
+                                                {props.name}
+                                            </label>
+                                            <div className='vr'></div>
+                                            <label className='m-3'>
+                                                {props.id}
+                                            </label>
+                                            <div className='vr'></div>
+                                            <button onClick={props.onModalClose} className='m-3 btn btn-outline-primary'>close</button>
+                                            <div className='col-md-12 border h-75'>
+                                                afafa
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='row m-0 h-25'>
+                                        <div className='col-md-3'>
+                                            height
+                                        </div>
+                                        <div className='col-md-3'>
+                                            weight
+                                        </div>
+                                        <div className='col-md-3'>
+                                            genders
+                                        </div>
+                                        <div className='col-md-3'>
+                                            egg grops
+                                        </div>
+                                        <div className='col-md-3'>
+                                            ablities
+                                        </div>
+                                        <div className='col-md-3'>
+                                            types
+                                        </div>
+                                        <div className='col-md-3'>
+                                            weak against
+                                        </div>
+                                    </div>
+                                    <div className='row m-0 h-25'>
+                                        stats
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
     )
 }
 
